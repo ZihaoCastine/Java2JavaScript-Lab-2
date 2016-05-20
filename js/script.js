cbb6db487d5236741dd88d684d1a33b0d056c168
Zihao Castine <zihaocastine@Zipcoder02s-MacBook-Pro.local>
2016-05-20 17:53:35 -0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 " use strict ";
 
+class Pet{
+  constructor(type, name){
+    this.type=type;
+    this.name=name;
+  }
+  speak(){
+    display.innerHTML+=this.type + "speaking<br/>";
+  }
+}
+
+class Dog extends Pet{
+  constructor(name){
+    super(Dog, name);
+  }
+  speak(){
+    display.innerHTML+="woof<br/>";
+  }
+}
+
+class Cat extends Pet{
+  constructor(name){
+    super(Cat, name);
+  }
+  speak(){
+    display.innerHTML+="meow<br/>";
+  }
+}
+
+class Bird extends Pet{
+  constructor(name){
+    super(Bird, name);
+  }
+  speak(){
+    display.innerHTML+="tweet<br/>";
+  }
+}
 function PetChat(){
   var display = document.getElementById("display");
   var numPets = null;
@@ -12,8 +48,18 @@ function PetChat(){
     for(var i=0; i<numPets; i++){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
+      var textOut = type + " " + name + " ";
       display.innerHTML += textOut;
+      switch (type.toLowerCase()) {
+        case "dog": new Dog(name).speak();
+          break;
+        case "cat": new Cat(name).speak();
+          break;
+        case "bird": new Bird(name).speak();
+          break;
+        default:
+          new Pet(type, name).speak();
+      }
     }
   };
 
